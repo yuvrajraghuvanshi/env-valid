f3d49cd9edb5ad1b55abf7848c7d3ca16e51602d
Yuvraj Raghuvanshi <124128352+yuvrajraghuvanshi@users.noreply.github.com>
2025-03-30 18:02:18 +0530

    feat: added logger and validations

--- src/utils.ts
+++ src/utils.ts
@@ -15,18 +15,66 @@ export function convertToType(value:string,config:ValidationConfig):any {
         switch(type){
             case 'string':
                 converted = String(value);
+                if(config.pattern&& !config.pattern.test(converted)){
+                    throw new Error(`Invalid value for ${config.errorMessage || config.type}: ${value}`);
+                }
+                if(config.min && converted.length < config.min){
+                    throw new Error(`String should be atleast equal to ${config.min}`);
+                }
+                if(config.max && converted.length > config.max){
+                    throw new Error(`String should not exceed  ${config.max}`);
+                }
                 break;
             case 'boolean':
-                converted = Boolean(value);
+                const lower=value.toLowerCase();
+                if(["true","y","yes","1"].includes(lower)){
+                    converted = true;
+                }
+                else if(["false","n","no","0"].includes(lower)){
+                    converted = false;
+                }
+                else{
+                    throw new Error(`Invalid boolean value for ${config.errorMessage || config.type}: ${value}`);
+                }
+               
                 break;
             case 'number':
                 converted = Number(value);
+                if(isNaN(converted)){
+                    throw new Error(`Invalid number value for ${config.errorMessage || config.type}: ${value}`);
+                }
+                if(config.min && converted < config.min){
+                    throw new Error(`Number should be atleast equal to ${config.min}`);
+                }
+                if(config.max && converted > config.max){
+                    throw new Error(`Number should not exceed  ${config.max}`);
+                }
                 break;
             case 'array':
-                converted = Array.isArray(value) ? value : (value.split(',').map(item=>convertToType(item,config.items)));
+              converted=value.startsWith('[')? JSON.parse(value): value.split(',').map(v=>v.trim());
+              if(config.items){
+                    converted.forEach((item:any)=>convertToType(item,config.items));
+                }
+                if (config.min !== undefined && converted.length < config.min) {
+                    throw new Error(`Array should have at least ${config.min} items`);
+                  }
+                  if (config.max !== undefined && converted.length > config.max) {
+                    throw new Error(`Array should not have more than ${config.max} items`);
+                  }
                 break;
             case 'object':
                 converted = typeof value === 'object' ? value : (JSON.parse(value));
+                if(config.properties){
+                    for(const [propKey,propConfig] of Object.entries(config.properties)){
+                        if(converted[propKey]!==undefined){
+                          converted[propKey]=  convertToType(converted[propKey],propConfig);
+                        }else if (propConfig.required) {
+                            throw new Error(`Required property ${propKey} is missing`);
+                          } else if (propConfig.default !== undefined) {
+                            converted[propKey] = propConfig.default;
+                          }
+                    }
+                }
                 break;
             case 'enum':
                 if(!config.enum.includes(value)){
@@ -43,4 +91,31 @@ export function convertToType(value:string,config:ValidationConfig):any {
     catch(error){
         throw new Error(`Invalid value for ${config.errorMessage || config.type}: ${value}`);
     }
+}
+
+export function logger(level:string,message:string,options:{logLevel:string}){
+    if (options.logLevel === 'none') return;
+  const levels={
+    error: 0,
+    warn: 1,
+    info: 2
+  }
+   if(levels[level]<=levels[options.logLevel]){
+    const prefix=`[env-validator] ${level.toUpperCase}`;
+
+    switch(level){
+        case 'error':
+        console.error(prefix, message);
+        break;
+        case 'warn':
+        console.warn(prefix, message);
+        break;
+        case 'info':
+        console.info(prefix, message);
+        break;
+        default:
+        console.log(prefix, message);
+        break;
+    }
+   }
 }
